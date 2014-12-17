a57bee2e945e40cd4ce0bd5fe891e9f1cd274a66
George Hilliard <gh403@msstate.edu>
2014-12-16 18:51:09 -0600

    Sleep to give bootloader time to waken

--- bullycpp.cpp
+++ bullycpp.cpp
@@ -22,6 +22,7 @@ int main(int argc, char** argv)
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		tty.setRTS(false);
 		tty.setDTR(false);
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 		std::cout << "OK\nInitializing PicBootloaderDriver... ";
 
