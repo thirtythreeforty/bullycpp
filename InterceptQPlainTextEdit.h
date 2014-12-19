4cfe1bf25496b7e402078f0907bf5ca51696749d
George Hilliard <gh403@msstate.edu>
2014-12-19 12:38:36 -0600

    Intercept paste events on the serial window
    This also allows drag/dropping a file onto the window to have its
    contents sent.

--- InterceptQPlainTextEdit.h
+++ InterceptQPlainTextEdit.h
@@ -1,6 +1,8 @@
 #ifndef INTERCEPTQPLAINTEXTEDIT_H
 #define INTERCEPTQPLAINTEXTEDIT_H
 
+#include <QFile>
+#include <QMimeData>
 #include <QPlainTextEdit>
 
 class InterceptQPlainTextEdit : public QPlainTextEdit
@@ -20,6 +22,17 @@ protected:
 	virtual void keyPressEvent(QKeyEvent *e) {
 		// Do not forward this upward (we don't want local echo)
 		emit keyPressed(e->text());
+	}
+	virtual void insertFromMimeData(const QMimeData *source) {
+		if(source->hasUrls()) {
+			for(const auto& url: source->urls()) {
+				QFile file(url.toLocalFile());
+				if(file.open(QIODevice::ReadOnly))
+					emit keyPressed(file.readAll());
+			}
+		}
+		else
+			emit keyPressed(source->text());
 	}
 };
 
